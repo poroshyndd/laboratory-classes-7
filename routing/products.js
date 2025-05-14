c795d72e25ed82389a0dd018f8542752344ff1ce
poroshyndd <poroshyndd@gmail.com>
2025-05-14 17:36:13 +0200

    Update products.js

--- routing/products.js
+++ routing/products.js
@@ -1,15 +1,31 @@
-const express = require("express");
-
-const productsController = require("../controllers/productsController");
-const cartController = require("../controllers/cartController");
+const express = require('express');
+const Product = require('../models/Product');
 
 const router = express.Router();
 
-router.get("/", productsController.getProductsView);
-router.get("/add", productsController.getAddProductView);
-router.post("/add", cartController.addProductToCart);
-router.get("/new", productsController.getNewProductView);
-router.get("/:name", productsController.getProductView);
-router.delete("/:name", productsController.deleteProduct);
+router.post('/', (req, res) => {
+  const { title, price, id } = req.body;
+  new Product(title, price, id).save()
+    .then(result => res.status(201).json(result))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.get('/', (req, res) => {
+  Product.fetchAll()
+    .then(products => res.json(products))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.get('/:id', (req, res) => {
+  Product.findById(req.params.id)
+    .then(prod => prod ? res.json(prod) : res.status(404).json({ error: 'Nie znaleziono produktu' }))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.delete('/:id', (req, res) => {
+  Product.deleteById(req.params.id)
+    .then(() => res.sendStatus(204))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
 
 module.exports = router;
