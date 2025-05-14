aaa37510815e6d03032760aa6c28ed8b8abf8367
poroshyndd <poroshyndd@gmail.com>
2025-05-14 17:35:50 +0200

    Update home.js

--- routing/home.js
+++ routing/home.js
@@ -1,9 +1,24 @@
-const express = require("express");
-
-const homeController = require("../controllers/homeController");
+const express = require('express');
+const { MENU_LINKS } = require('../constants/navigation');
+const { STATUS_CODE } = require('../constants/statusCode');
+const cartController = require('../controllers/cartController');
 
 const router = express.Router();
 
-router.get("/", homeController.getHomeView);
+router.get('/', async (req, res, next) => {
+  try {
+    const cartCount = await cartController.getProductsCount('default');
+    res
+      .status(STATUS_CODE.OK)
+      .render('home', {
+        headTitle: 'Strona główna',
+        menuLinks: MENU_LINKS,
+        activeLinkPath: '/',
+        cartCount
+      });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
