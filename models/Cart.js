fdf41354d72e7900ad84acbf5b9a54706b0fd2d1
poroshyndd <poroshyndd@gmail.com>
2025-05-14 17:33:58 +0200

    Update Cart.js

--- models/Cart.js
+++ models/Cart.js
@@ -1,56 +1,53 @@
-const Product = require("./Product");
+const { getDatabase } = require('../database');
+const COLLECTION_NAME = 'carts';
 
 class Cart {
-  constructor() {}
-
-  static #items = [];
-
-  static add(productName) {
-    const product = Product.findByName(productName);
-
-    if (!product) {
-      throw new error(`Product '${productName}' not found.`);
-    }
-
-    if (!this.#items.length) {
-      this.#items.push({ product, quantity: 1 });
-
-      return;
-    }
-
-    const existingProduct = this.#items.find(
-      (item) => item.product.name === productName
-    );
-
-    if (existingProduct) {
-      existingProduct.quantity += 1;
-    } else {
-      this.#items.push({ product, quantity: 1 });
-    }
+  constructor(id) {
+    this.id = id;
   }
 
-  static getItems() {
-    return this.#items;
+  addProduct(productId, quantity) {
+    const db = getDatabase();
+    return db.collection(COLLECTION_NAME)
+      .findOne({ _id: this.id })
+      .then(cart => {
+        if (!cart) {
+          return db.collection(COLLECTION_NAME)
+            .insertOne({ _id: this.id, items: [{ productId, quantity }] });
+        }
+        const items = cart.items || [];
+        const idx = items.findIndex(i => i.productId === productId);
+        if (idx >= 0) {
+          items[idx].quantity += quantity;
+        } else {
+          items.push({ productId, quantity });
+        }
+        return db.collection(COLLECTION_NAME)
+          .updateOne({ _id: this.id }, { $set: { items } });
+      });
   }
 
-  static getProductsQuantity() {
-    if (!this.#items?.length) {
-      return 0;
-    }
-
-    return this.#items.reduce((total, item) => {
-      return total + item.quantity;
-    }, 0);
+  getCart() {
+    const db = getDatabase();
+    return db.collection(COLLECTION_NAME)
+      .findOne({ _id: this.id });
   }
 
-  static getTotalPrice() {
-    return this.#items.reduce((total, item) => {
-      return total + item.product.price * item.quantity;
-    }, 0);
+  deleteProduct(productId) {
+    const db = getDatabase();
+    return this.getCart()
+      .then(cart => {
+        if (!cart) throw new Error('Cart not found.');
+        const items = cart.items.filter(i => i.productId !== productId);
+        return db.collection(COLLECTION_NAME)
+          .updateOne({ _id: this.id }, { $set: { items } });
+      });
   }
 
-  static clearCart() {
-    this.#items = [];
+  static clearCart(cartId) {
+    const db = getDatabase();
+    return db.collection(COLLECTION_NAME)
+      .deleteOne({ _id: cartId });
   }
 }
 
